435d8170b9b120ccda671f4e4dcf4083be89cd8e
Megan McMahon <mcmahon.meganelizabeth@gmail.com>
2022-08-24 21:50:30 -0600

    Add nearest Fuel Station Scaffold

---
+++ NearestFuelStation/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NearestFuelStation.Models;
+
+namespace NearestFuelStation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new LocationViewModel());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NearestFuelStation/Models/LocationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace NearestFuelStation.Models
+{
+    public class LocationViewModel
+    {
+        public string Location { get; set; } = "Turing";
+
+        public List<SelectListItem> Locations { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Text = "Turing", Value = "1331 17th St LL100, Denver, CO 80202" }
+        };
+    }
+}
